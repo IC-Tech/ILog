1f9112a9dca8dcbcf995bdb8a43d5466f3ba68cb
IC-Tech <ic.imesh.chamara@gmail.com>
2019-07-24 01:24:13 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,32 @@ import ReactDOM from 'react-dom'
 import './style.css'
 import './Themes.css'
 
+var Data = [
+	{
+		name: 'Title 01',
+		timeC: 1563895945931,
+		timeM: 1563895970135,
+		content: 'The Content 001.'
+	},
+	{
+		name: 'Title 02',
+		timeC: 1563895945931,
+		timeM: 1563895970135,
+		content: 'The Content 002.'
+	}
+]
+
 class ILog extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
+			UI: 0
 		}
 		this.SetMent = this.SetMent.bind(this)
+		this.EditCall = this.EditCall.bind(this)
+		this.EditActon = this.EditActon.bind(this)
 		this.backAction = null
+		this.i = -1
 	}
   componentDidMount() {
   	var a = () => {
@@ -32,6 +51,30 @@ class ILog extends Component {
   	document.querySelector('.menu').style.display = v == true ? 'block' : 'none'
   	this.backAction = v ? (() => this.SetMent(0)) : null
   }
+  EditCall(i, e) {
+  	const c = a => a.toString().length == 1 ? '0' + a : a.toString()
+  	const a = a => `${a.getFullYear()}-${c(a.getMonth() + 1)}-${c(a.getDate())}`
+  	const b = a => `${c(a.getHours())}:${c(a.getMinutes())}`
+		document.querySelector('#i1').value = i == -1 ? '' : Data[i].name
+		document.querySelector('#i2').value = a(new Date(i == -1 ? Date.now() : Data[i].timeC))
+		document.querySelector('#i3').value = b(new Date(i == -1 ? Date.now() : Data[i].timeC))
+		document.querySelector('#i4').value = a(new Date(i == -1 ? Date.now() : Data[i].timeM))
+		document.querySelector('#i5').value = b(new Date(i == -1 ? Date.now() : Data[i].timeM))
+		document.querySelector('#i6').value = i == -1 ? '' : Data[i].content
+  	this.setState({UI: 1})
+  	this.SetMent(false)
+  	this.i = i
+  }
+  EditActon(v) {
+  	if(v == 1)
+	  	Data[this.i == -1 ? Data.length : this.i] = {
+	  		name: document.querySelector('#i1').value,
+	  		timeC: Date.parse(document.querySelector('#i2').value + ' ' + document.querySelector('#i3').value),
+	  		timeM: Date.parse(document.querySelector('#i4').value + ' ' + document.querySelector('#i5').value),
+	  		content: document.querySelector('#i6').value
+	  	}
+  	this.setState({UI: 0})
+  }
 	render() {
 		return (
 			<div id='ILog' className='ICApp'>
@@ -42,34 +85,35 @@ class ILog extends Component {
 						<span>ILog</span>
 					</div>
 				</div>
-				<div className='Entry'>{(()=>{
-					var r = []
-					for(var i=0; i<30; i++)
-						r[i] = <div key={'k' + i}><span className='c1'>Title {i + 1}</span><span className='c2'>Content {i + 1}</span><span className='c3'>{Date().toString()}</span></div>
-					return r
-				})()}</div>
-				<div className='Editor' style={{display: 'none'}}>
+				<div className='Entry' style={{display: this.state.UI == 0 ? 'flex' : 'none'}}>{
+					Data.map((v, i) => <div key={'k' + i + v.timeC} onClick={(e) => this.EditCall(i, e)}><span className='c1'>{v.name}</span><span className='c2'>{v.content}</span><span className='c3'>{new Date(v.timeM).toString()}</span></div>)
+				}</div>
+				<div className='Editor' style={{display: this.state.UI == 1 ? 'flex' : 'none'}}>
 					<div className='c2'>
-						<label>Name:<input type='text'/></label>
+						<label>Name:<input id='i1' type='text'/></label>
 					</div>
 					<div className='c1'>
-						<label>Create Date:<input type='date'/></label>
+						<label>Create Date:<input id='i2' type='date'/></label>
 					</div>
 					<div className='c1'>
-						<label>Create Time:<input type='time'/></label>
+						<label>Create Time:<input id='i3' type='time'/></label>
 					</div>
 					<div className='c1'>
-						<label>Last Update Date:<input type='date'/></label>
+						<label>Last Update Date:<input id='i4' type='date'/></label>
 					</div>
 					<div className='c1'>
-						<label>Last Update Time:<input type='time'/></label>
+						<label>Last Update Time:<input id='i5' type='time'/></label>
 					</div>
-					<textarea></textarea>
+					<textarea id='i6'></textarea>
+					<div>
+						<button onClick={() => this.EditActon(0)}>CANCEL</button>
+						<button onClick={() => this.EditActon(1)} className='s1'>SAVE</button>
+					</div>
 				</div>
 				<div className='menu'>
 					<div className='c1'>
 						<div>
-							<button>Create New</button>
+							<button onClick={e => this.EditCall(-1, e)}>Create New</button>
 							<button>Import</button>
 							<button>Export</button>
 							<button onClick={window.close}>Exit</button>
