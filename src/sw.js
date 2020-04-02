096f7f3fd7ada873da5cd902e333ff36854d72ec
IC-Tech <ic.imesh.chamara@gmail.com>
2020-04-03 04:50:35 +0530

    bug fix

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,7 @@
 const _caches_main = 'ic-c-v1';
 const _caches_ = {
   app: {
-    name: _caches_main + '-app-v2',
+    name: _caches_main + '-app-v3',
     data: [ '/p201907221623.bundle.js', '/style/p201907221623.css' ]
   },
   runtime: {
@@ -10,8 +10,16 @@ const _caches_ = {
     data: [ '/' ]
   }
 };
-
-self.addEventListener('install', e => Object.keys(_caches_).forEach(a => e.waitUntil(caches.open(_caches_[a].name).then(v => _caches_[a].data.forEach(b => fetch(new Request(new URL(self.location.origin + b + '?t=' + Date.now()))).then(a => v.put(b, a)))))));
+self.addEventListener('install', e => {
+  Object.keys(_caches_).forEach(a => e.waitUntil(caches.open(_caches_[a].name).then(v => _caches_[a].data.forEach(b => fetch(new Request(new URL(self.location.origin + b + '?t=' + Date.now()))).then(a => v.put(b, a))))))
+  e.waitUntil(new Promise(async (b, a) => {
+    a = Object.keys(_caches_).map(_ => _caches_[_].name);
+    var c = [];
+    (await caches.keys()).forEach(_ => a.indexOf(_) >= 0 ? 0: c.push(caches.delete(_)));
+    await Promise.all(c);
+    b();
+  }));
+});
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(v => Promise.all(v.map(v => { if(!v.startsWith(_caches_main)) return caches.delete(v) })))));
 
 self.addEventListener('fetch', e => {
